e9f4407e0a48bc47aa45d94eab2e504ab8e5d74a
m0cruz <marlonjc44@gmail.com>
2026-06-27 11:58:39 -0300

    Implementing level 2 Implementing persistent score between levels

--- code/Const.py
+++ code/Const.py
@@ -1,30 +1,36 @@
+# C
 import pygame
 
-# C
-
 C_ORANGE = (255, 128, 0)
+C_YELLOW = (255, 255, 128)
 C_WHITE = (255, 255, 255)
-C_YELLOW = (255, 255, 0)
 C_GREEN = (0, 128, 0)
 C_CYAN = (0, 128, 128)
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
-ENTITY_SPEED = {'Level1Bg0': 0,
-                'Level1Bg1': 1,
-                'Level1Bg2': 2,
-                'Level1Bg3': 3,
-                'Level1Bg4': 4,
-                'Level1Bg5': 5,
-                'Level1Bg6': 6,
-                'Player1': 3,
-                'Player1Shot': 1,
-                'Player2': 3,
-                'Player2Shot': 1,
-                'Enemy1': 2,
-                'Enemy1Shot': 4,
-                'Enemy2': 1,
-                'Enemy2Shot': 4
+EVENT_TIMEOUT = pygame.USEREVENT + 2
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5,
+    'Level1Bg6': 6,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
+    'Player1': 3,
+    'Player1Shot': 1,
+    'Player2': 3,
+    'Player2Shot': 3,
+    'Enemy1': 1,
+    'Enemy1Shot': 5,
+    'Enemy2': 1,
+    'Enemy2Shot': 2,
 }
 
 ENTITY_HEALTH = {
@@ -35,6 +41,11 @@ ENTITY_HEALTH = {
     'Level1Bg4': 999,
     'Level1Bg5': 999,
     'Level1Bg6': 999,
+    'Level2Bg0': 999,
+    'Level2Bg1': 999,
+    'Level2Bg2': 999,
+    'Level2Bg3': 999,
+    'Level2Bg4': 999,
     'Player1': 300,
     'Player1Shot': 1,
     'Player2': 300,
@@ -45,29 +56,27 @@ ENTITY_HEALTH = {
     'Enemy2Shot': 1,
 }
 
-ENTITY_SHOT_DELAY = {
-    'Player1': 15,
-    'Player2': 15,
-    'Enemy1': 30,
-    'Enemy2': 50
-}
-
 ENTITY_DAMAGE = {
-    'Level1Bg0': 999,
-    'Level1Bg1': 999,
-    'Level1Bg2': 999,
-    'Level1Bg3': 999,
-    'Level1Bg4': 999,
-    'Level1Bg5': 999,
-    'Level1Bg6': 999,
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
     'Player1': 1,
     'Player1Shot': 25,
     'Player2': 1,
     'Player2Shot': 20,
     'Enemy1': 1,
-    'Enemy1Shot': 20,
+    'Enemy1Shot': 60,
     'Enemy2': 1,
-    'Enemy2Shot': 15,
+    'Enemy2Shot': 80,
 }
 
 ENTITY_SCORE = {
@@ -78,6 +87,11 @@ ENTITY_SCORE = {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
     'Player1': 0,
     'Player1Shot': 0,
     'Player2': 0,
@@ -88,6 +102,12 @@ ENTITY_SCORE = {
     'Enemy2Shot': 0,
 }
 
+ENTITY_SHOT_DELAY = {
+    'Player1': 20,
+    'Player2': 15,
+    'Enemy1': 100,
+    'Enemy2': 200,
+}
 
 # M
 MENU_OPTION = ('NEW GAME 1P',
@@ -108,9 +128,29 @@ PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
 PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
                     'Player2': pygame.K_LCTRL}
 
-#S
+# S
 SPAWN_TIME = 4000
 
+# T
+TIMEOUT_STEP = 100  # 100ms
+TIMEOUT_LEVEL = 20000  # 20s
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
+
+# S
+SCORE_POS = {'Title': (WIN_WIDTH / 2, 50),
+             'EnterName': (WIN_WIDTH / 2, 80),
+             'Label': (WIN_WIDTH / 2, 90),
+             'Name': (WIN_WIDTH / 2, 110),
+             0: (WIN_WIDTH / 2, 110),
+             1: (WIN_WIDTH / 2, 130),
+             2: (WIN_WIDTH / 2, 150),
+             3: (WIN_WIDTH / 2, 170),
+             4: (WIN_WIDTH / 2, 190),
+             5: (WIN_WIDTH / 2, 210),
+             6: (WIN_WIDTH / 2, 230),
+             7: (WIN_WIDTH / 2, 250),
+             8: (WIN_WIDTH / 2, 270),
+             9: (WIN_WIDTH / 2, 290),
+             }
